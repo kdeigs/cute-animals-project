5bf2f3db0a9e50e89372e05cc153a91c652e60ab
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-03-03 09:23:34 -0600

    Added session capabilities

---
+++ controllers/sessions_controller.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const express = require('express');
+const sessions = express.Router();
+const User = require('../models/users.js');
+
+sessions.get('/new', (req, res) => {
+    res.render('sessions/new.ejs', {
+        currentUser: req.session.currentUser
+    });
+});
+
+User.findOne({username: req.body.username}, (err, foundUser) => {
+    if (err) {
+        console.log(err);
+        res.send('The db had a problem');
+    }else if (!foundUser){
+        res.send('<a href="/">Sorry, no user found </a>');
+    }else {
+        if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+            req.session.currentUser = foundUser;
+        }else{
+            res.send('<a href="/">Sorry, password does not match</a>')
+        }
+    }
+});
+
+sessions.delete('/', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
+
+module.exports = sessions;
